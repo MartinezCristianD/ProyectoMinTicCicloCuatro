2bd0e1cd50a444704e1b3e033e7c618884c10f19
davick <davick16@gmail.com>
2021-10-29 14:47:46 -0500

    splashscreen

--- app/src/main/java/com/proyectomintic/stockerinv/MainActivity.java
+++ app/src/main/java/com/proyectomintic/stockerinv/MainActivity.java
@@ -8,7 +8,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_AppCompat);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
