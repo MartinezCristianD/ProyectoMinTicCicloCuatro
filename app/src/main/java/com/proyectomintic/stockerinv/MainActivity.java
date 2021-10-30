81bfe4e828ba850315a2d8a45cb748889a669b5e
davick <davick16@gmail.com>
2021-10-30 09:41:54 -0500

    pagina de registro parte 2

--- app/src/main/java/com/proyectomintic/stockerinv/MainActivity.java
+++ app/src/main/java/com/proyectomintic/stockerinv/MainActivity.java
@@ -2,7 +2,9 @@ package com.proyectomintic.stockerinv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button registrarse = findViewById(R.id.botonRegistro);
+        registrarse.setOnClickListener(v -> {
+            Intent intent = new Intent (v.getContext(), RegistroActivity.class);
+            startActivity(intent);
+        });
 
     }
 }
--- app/src/main/java/com/proyectomintic/stockerinv/RegistroActivity.java
+++ app/src/main/java/com/proyectomintic/stockerinv/RegistroActivity.java
@@ -2,7 +2,9 @@ package com.proyectomintic.stockerinv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class RegistroActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class RegistroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        ImageButton botonRegresar = findViewById(R.id.botonRegresar);
+        botonRegresar.setOnClickListener(v -> {
+            Intent intent = new Intent (v.getContext(), MainActivity.class);
+            startActivity(intent);
+        });
     }
 }
