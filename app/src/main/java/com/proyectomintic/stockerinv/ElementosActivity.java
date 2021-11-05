fc0ac8bbb8656cf99dda728c83d1224fd1382d49
davick <davick16@gmail.com>
2021-11-04 22:15:27 -0500

    activity agregar elementos realizada
    Signed-off-by: davick <davick16@gmail.com>

--- app/src/main/java/com/proyectomintic/stockerinv/ElementosActivity.java
+++ app/src/main/java/com/proyectomintic/stockerinv/ElementosActivity.java
@@ -3,6 +3,8 @@ package com.proyectomintic.stockerinv;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,13 +16,38 @@ public class ElementosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_elementos);
 
+        TextView mostrarArticulosSeekBar = (TextView) findViewById(R.id.textViewContador);
+        SeekBar barraAgregarArticulos;
 
-        ImageButton botonregresar = findViewById(R.id.imageButton);
-        botonregresar.setOnClickListener(v -> {
+        ImageButton botonRegresar = findViewById(R.id.botonAtras);
+        botonRegresar.setOnClickListener(v -> {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         });
 
+        // SeekBar
+        barraAgregarArticulos = (SeekBar) findViewById(R.id.seekBar);
+        // Valor Inicial
+        barraAgregarArticulos.setProgress(0);
+        // Valor Final
+        barraAgregarArticulos.setMax(20);
+        barraAgregarArticulos.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    //hace un llamado a la perilla cuando se arrastra
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        String textoProgressBar = getString(R.string.texto_unidades);
+                        mostrarArticulosSeekBar.setText(progress + textoProgressBar);
+                    }
+
+                    //hace un llamado  cuando se toca la perilla
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+                    }
+
+                    //hace un llamado  cuando se detiene la perilla
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                    }
+                });
     }
 
     @Override
@@ -30,3 +57,7 @@ public class ElementosActivity extends AppCompatActivity {
 
     }
 }
+
+
+
+
