d3d48675d7320615c760a5a179e64805386a42ae
davick <davick16@gmail.com>
2021-11-07 14:21:38 -0500

    manejo de tados entre activitys

--- app/src/main/java/com/proyectomintic/stockerinv/views/ElementosActivity.java
+++ app/src/main/java/com/proyectomintic/stockerinv/views/ElementosActivity.java
@@ -2,59 +2,55 @@ package com.proyectomintic.stockerinv.views;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.SeekBar;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.proyectomintic.stockerinv.R;
 import com.proyectomintic.stockerinv.databinding.ActivityElementosBinding;
-import com.proyectomintic.stockerinv.databinding.ActivityMainBinding;
 
 public class ElementosActivity extends AppCompatActivity {
+
+    // Variables
     private ActivityElementosBinding binding;
     AutoCompleteTextView listaCategorias;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityElementosBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        // Llenando la lista  para seleccionar la categoria
         listaCategorias = ((AutoCompleteTextView) binding.textViewCategoriaElegida.getEditText());
          if(listaCategorias != null){
-             listaCategorias.setAdapter(new ArrayAdapter<String>(this, android.R.layout.select_dialog_item,getResources().getStringArray(R.array.categorias)));
+             listaCategorias.setAdapter(new ArrayAdapter<>(this, android.R.layout.select_dialog_item, getResources().getStringArray(R.array.categorias)));
          }
 
-
-        TextView mostrarArticulosSeekBar = findViewById(R.id.textViewContador);
-        SeekBar barraAgregarArticulos;
-
-
-        Button btnCrearElemento = findViewById(R.id.btnCrearElementos);
-        btnCrearElemento.setOnClickListener(v -> {
+        // Evento click  para crear los Elementos
+        binding.btnCrearElementos.setOnClickListener(v -> {
             Intent intent = new Intent(this, InventarioActivity.class);
             startActivity(intent);
         });
 
-        // SeekBar
-        barraAgregarArticulos = findViewById(R.id.seekBar);
-        // Valor Inicial
-        barraAgregarArticulos.setProgress(0);
+        // SeekBar Valor Inicial
+        binding.seekBar.setProgress(0);
+
         // Valor Final
-        barraAgregarArticulos.setMax(20);
-        barraAgregarArticulos.setOnSeekBarChangeListener(
+        binding.seekBar.setMax(20);
+
+        // Evento estado de cambio de la barra inferior
+        binding.seekBar.setOnSeekBarChangeListener(
                 new SeekBar.OnSeekBarChangeListener() {
+
                     //hace un llamado a la perilla cuando se arrastra
+
                     @Override
                     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                         String textoProgressBar = getString(R.string.texto_unidades);
-                        mostrarArticulosSeekBar.setText(progress + " " + textoProgressBar);
+                        binding.textViewContador.setText(progress + " " + textoProgressBar);
                     }
 
                     //hace un llamado  cuando se toca la perilla
