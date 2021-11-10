154b49e9cae9007e958410cfbe08070c812de4fb
atene <atenea1287@hotmail.com>
2021-11-09 21:24:00 -0500

    commit inicial

--- app/src/main/java/com/proyectomintic/stockerinv/views/ElementosActivity.java
+++ app/src/main/java/com/proyectomintic/stockerinv/views/ElementosActivity.java
@@ -16,6 +16,7 @@ public class ElementosActivity extends AppCompatActivity {
     private ActivityElementosBinding binding;
     AutoCompleteTextView listaCategorias;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
